f67f48f07400317c3b38595c8044965bb1772cf0
SCP-079 <scp-079@archlinux.localdomain>
2024-11-12 10:07:19 +0200

    Some remapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,8 +4,9 @@ require("nvchad.mappings")
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "<C-u>", "<cmd> UndotreeToggle <CR>", { desc = "Togglel Undotree" })
+map("n", "<C-i>", "<cmd> UndotreeToggle <CR>", { desc = "Togglel Undotree" })
+map("n", "<C-d", "<C-d>zz")
+map("n", "<C-u", "<C-u>zz")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
